9d2cb2b9b2b25b6c283dcd477fc871947cdf1c29
zaifrun <martinknud@gmail.com>
2021-02-12 14:03:46 +0100

    updated gradle

--- app/src/main/java/org/pondar/imageviewdemokotlin/MainActivity.kt
+++ app/src/main/java/org/pondar/imageviewdemokotlin/MainActivity.kt
@@ -2,6 +2,7 @@ package org.pondar.imageviewdemokotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
